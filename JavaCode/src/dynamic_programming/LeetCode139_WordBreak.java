9b2ff642659cafb5777a078f4043ab645dd8677c
Rundong <rzhu16@asu.edu>
2020-01-29 17:49:10 -0800

    commit test

---
+++ JavaCode/src/dynamic_programming/LeetCode139_WordBreak.java
@@ -0,0 +1,10 @@
+package dynamic_programming;
+
+public class LeetCode139_WordBreak {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World");
+    }
+
+}
