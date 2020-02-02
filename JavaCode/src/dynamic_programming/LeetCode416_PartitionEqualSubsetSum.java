ba7899fce0d2790f35894d821487b3177c6bb092
Rundong <rzhu16@asu.edu>
2020-02-01 22:08:54 -0800

    LeetCode 416

--- JavaCode/src/dynamic_programming/LeetCode416_PartitionEqualSubsetSum.java
+++ JavaCode/src/dynamic_programming/LeetCode416_PartitionEqualSubsetSum.java
@@ -32,38 +32,24 @@ public class LeetCode416_PartitionEqualSubsetSum {
 
         int n = nums.length;
         
-        boolean[][] dp = new boolean[n + 1][sum + 1];
-        
-        for (int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], false);
-        }
+        boolean[] dp = new boolean[sum + 1];
     
-        dp[0][0] = true;
+        dp[0] = true;
 
-        for (int i = 1; i < n + 1; i++) {
-            dp[i][0] = true;
-        }
-        for (int j = 1; j < sum + 1; j++) {
-            dp[0][j] = false;
-        }
-
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j <= sum; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j >= nums[i - 1]) {
-                    dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
-                }
+        for (int i = 1; i <= n; i++) {
+            for (int j = sum; j >= nums[i - 1]; j--) {
+                dp[j] = dp[j] || dp[j - nums[i - 1]]; //dp[j] means select this num, dp[j - nums[i - 1]] means select this num
             }
         }
    
-        return dp[n][sum];
+        return dp[sum];
     }
 
     public static void main(String[] args) {
         //
         LeetCode416_PartitionEqualSubsetSum obj = new LeetCode416_PartitionEqualSubsetSum();
         
-        int[] nums = {1, 5, 11, 5};
+        int[] nums = {1, 5, 13, 5};
         
         System.out.println(obj.canPartition(nums));
     }
