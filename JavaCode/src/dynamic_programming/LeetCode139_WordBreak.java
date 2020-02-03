5d6d1b724195b553e7eb582d802b9de5cb706716
Rundong <rzhu16@asu.edu>
2020-02-02 18:50:26 -0800

    LeetCode 139

--- JavaCode/src/dynamic_programming/LeetCode139_WordBreak.java
+++ JavaCode/src/dynamic_programming/LeetCode139_WordBreak.java
@@ -1,5 +1,10 @@
 package dynamic_programming;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * 
  * @author run-dong-zhu
@@ -16,9 +21,31 @@ package dynamic_programming;
  */
 public class LeetCode139_WordBreak {
 
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        
+        dp[0] = true;
+        
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[dp.length - 1];
+    }
+    
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("Hello World");
+        LeetCode139_WordBreak obj = new LeetCode139_WordBreak();
+        String s = "leetcode";
+        List<String> wordDict = new ArrayList<>();
+        wordDict.add("leet");
+        wordDict.add("code");
+        System.out.println(obj.wordBreak(s, wordDict));
     }
 
 }
