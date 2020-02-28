f164415019dab1b3a9a72cf341663616c8c9b405
rundong-usaclaymore <rundong@claymoreusa.com>
2020-02-28 10:58:57 -0800

    LeetCode 213

--- JavaCode/src/dynamic_programming/LeetCode213_HouseRobber2.java
+++ JavaCode/src/dynamic_programming/LeetCode213_HouseRobber2.java
@@ -19,7 +19,7 @@ public class LeetCode213_HouseRobber2 {
         
         int[] dp = new int[nums.length];
         dp[start] = nums[start];
-        dp[start + 1] = Math.max(nums[start], nums[end]);
+        dp[start + 1] = Math.max(nums[start], nums[start + 1]);
         
         for(int i = start + 2; i <= end; i++) {
             dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
