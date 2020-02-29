2ffe05223c152a0875d1bc48ff3eb214a615e77b
Rundong <rzhu16@asu.edu>
2020-02-28 16:54:39 -0800

    Quick Sort

--- JavaCode/src/sorting_problems/QuickSort.java
+++ JavaCode/src/sorting_problems/QuickSort.java
@@ -1,10 +1,54 @@
 package sorting_problems;
-
+/**
+ * 
+ * @author run-dong-zhu
+ * @description Quick Sort
+ *
+ * Time Complexity:
+ * BEST: O(nlogn) -- pivot middle element 
+ * AVG: O(nlogn)
+ * WORST: O(n2) -- pivot biggest or smallest element
+ */
 public class QuickSort {
+    
+    public int partition(int[] nums, int low, int high) {
+        int pivot = nums[high];
+        int i = low - 1;
+        
+        for(int j = low; j < high; j++) {
+            if(nums[j] < pivot) {
+                i++;
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        
+        int temp = nums[i + 1];
+        nums[i + 1] = pivot;
+        nums[high] = temp;
+        
+        return i + 1;
+    }
+    
+    public void quickSort(int[] nums, int low, int high) {
+        if(low < high) {
+            int pivot = partition(nums, low, high);
+            // divide and conquer
+            quickSort(nums, low, pivot - 1);
+            quickSort(nums, pivot + 1, high);
+        }
+    }
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        QuickSort obj = new QuickSort();
+        int[] nums = {3, 1, 5, 2, 7, 4};
+        
+        obj.quickSort(nums, 0, nums.length - 1);
+        
+        for(int num : nums) {
+            System.out.print(num + " ");
+        }
     }
 
 }
