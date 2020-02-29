8b6fdc87e73d0bce7b3b5d6501286a3e70d1baf7
rundong-usaclaymore <rundong@claymoreusa.com>
2020-02-28 17:58:04 -0800

    LeetCode 23

---
+++ JavaCode/src/sorting_problems/LeetCode23_MergeKSortedLists.java
@@ -0,0 +1,36 @@
+package sorting_problems;
+
+/**
+ * 
+ * @author run-dong-zhu
+ * @description LeetCode 23, Level: Hard
+ *
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ * 
+ * Input:
+ * [
+ *  1->4->5,
+ *  1->3->4,
+ *  2->6
+ *  ]
+ *  
+ *  Output: 1->1->2->3->4->4->5->6
+ */
+public class LeetCode23_MergeKSortedLists {
+	
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+	
+	public ListNode mergeKLists(ListNode[] lists) {
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
