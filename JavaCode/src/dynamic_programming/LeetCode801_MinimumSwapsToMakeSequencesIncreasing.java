2047f34883783e3510776cb92f526993173f777f
Rundong <rzhu16@asu.edu>
2020-03-02 22:08:53 -0800

    LeetCode 801

--- JavaCode/src/dynamic_programming/LeetCode801_MinimumSwapsToMakeSequencesIncreasing.java
+++ JavaCode/src/dynamic_programming/LeetCode801_MinimumSwapsToMakeSequencesIncreasing.java
@@ -5,11 +5,13 @@ package dynamic_programming;
  * @author run-dong-zhu
  * @description LeetCode 801, Level: Medium
  * 
- * 
+ * https://leetcode.com/problems/minimum-swaps-to-make-sequences-increasing/
  */
 public class LeetCode801_MinimumSwapsToMakeSequencesIncreasing {
 	
 	public int minSwap(int[] A, int[] B) {
+	    // swapRecord means for the ith element in A and B, the minimum swaps if we swap A[i] and B[i]
+	    // fixRecord means for the ith element in A and B, the minimum swaps if we DONOT swap A[i] and B[i]
         int swapRecord = 1, fixRecord = 0;
         
         for (int i = 1; i < A.length; i++) {
@@ -35,8 +37,11 @@ public class LeetCode801_MinimumSwapsToMakeSequencesIncreasing {
 	public static void main(String[] args) {
 		LeetCode801_MinimumSwapsToMakeSequencesIncreasing obj = new LeetCode801_MinimumSwapsToMakeSequencesIncreasing();
 
-		int[] A = {0, 3, 5, 8, 9};
-		int[] B = {2, 1, 4, 6, 9};
+//		int[] A = {0, 3, 5, 8, 9};
+//		int[] B = {2, 1, 4, 6, 9};
+		
+		int[] A = {0, 4, 4, 5, 9};
+        int[] B = {0, 1, 6, 8, 10};
 		
 		System.out.println(obj.minSwap(A, B));
 	}
