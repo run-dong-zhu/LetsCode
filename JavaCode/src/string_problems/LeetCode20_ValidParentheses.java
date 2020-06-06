239d99bc63bdbc6278bc21ae8f9ea226a29b439b
run-dong-zhu <rzhu16@asu.edu>
2020-06-06 16:55:22 -0500

    LeetCode 20

---
+++ JavaCode/src/string_problems/LeetCode20_ValidParentheses.java
@@ -0,0 +1,40 @@
+package string_problems;
+
+import java.util.Stack;
+
+public class LeetCode20_ValidParentheses {
+    public boolean isValid(String s) {
+        if(s.length() % 2 != 0) {
+            return false;
+        } 
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(')');
+            }         
+            else if(s.charAt(i) == '{') {
+                stack.push('}');
+            }
+            else if(s.charAt(i) == '[') {
+                stack.push(']');
+            }
+            else {
+                if(stack.isEmpty() || stack.pop() != s.charAt(i)) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+    
+    public static void main(String[] args) {
+        String s = "(){[]}";
+        
+        LeetCode20_ValidParentheses obj = new LeetCode20_ValidParentheses();
+        
+        System.out.println(obj.isValid(s));
+    }
+}
